4e70493178df6583115dbcdf028593ad34f33304
Dima <kuzmichman92@gmail.com>
2020-07-02 23:02:55 +0300

    heroku11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_user, only: [:create, :show, :update]
+  before_action :authenticate_user!, only: [:create, :show, :update]
   
   def index
     @users = User.all
