0a694bb4fd9afe0212e46292a27366678b980b33
Dima <kuzmichman92@gmail.com>
2020-07-02 23:35:03 +0300

    heroku12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,49 +5,4 @@ class RoomsController < ApplicationController
     @users = User.all
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to action: :index
-    else
-      render 'validation error'
-    end
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      redirect_to action: :index
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'user was destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :email)
-  end
 end
